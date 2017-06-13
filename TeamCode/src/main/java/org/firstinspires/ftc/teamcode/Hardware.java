e45e802983681721d4e07636b6702643e566a236
Kosaro <okosarkosarewicz@gmail.com>
2017-06-13 19:23:26 -0400

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -13,8 +13,12 @@ import com.qualcomm.robotcore.robocol.RobocolDatagramSocket;
  */
 
 public class Hardware {
+    //Hardware Constants
+    final static double ENCODER_TICKS_PER_REVOLUTION = 1120;
+    final static double ROBOT_HEIGHT = 24.0;   //inches
+    final static double WHEEL_DIAMETER = 4.0;  //inches
+    final static double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
 
-    //Hello there testing
 
     //Configuration names
     final static String LEFT_MOTOR = "lm";
@@ -40,38 +44,42 @@ public class Hardware {
 
     private void initialize(HardwareMap hardwareMap) {
         leftMotor = hardwareMap.dcMotor.get(LEFT_MOTOR);
-        rightMotor = hardwareMap.dcMotor.get(RIGHT_MOTOR);
-        gyro = hardwareMap.gyroSensor.get(GYRO);
-        frontUltrasonic = hardwareMap.ultrasonicSensor.get(FRONT_ULTRASONIC);
-        rearUltrasonic = hardwareMap.ultrasonicSensor.get(REAR_ULTRASONIC);
+        //rightMotor = hardwareMap.dcMotor.get(RIGHT_MOTOR);
+        //gyro = hardwareMap.gyroSensor.get(GYRO);
+        //frontUltrasonic = hardwareMap.ultrasonicSensor.get(FRONT_ULTRASONIC);
+        //rearUltrasonic = hardwareMap.ultrasonicSensor.get(REAR_ULTRASONIC);
 
-        leftMotor.setDirection(LEFT_MOTOR_DIRECTION);
-        leftMotor.setDirection(RIGHT_MOTOR_DIRECTION);
+        //leftMotor.setDirection(LEFT_MOTOR_DIRECTION);
+        //leftMotor.setDirection(RIGHT_MOTOR_DIRECTION);
     }
 
 
-    void resetEncoders(){
+    void resetEncoders() {
         DcMotor.RunMode leftRunMode = leftMotor.getMode();
-        DcMotor.RunMode rightRunMode = rightMotor.getMode();
+        //DcMotor.RunMode rightRunMode = rightMotor.getMode();
         setMotorRunMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        setRightMotorRunMode(rightRunMode);
+        //setRightMotorRunMode(rightRunMode);
         setLeftMotorRunMode(leftRunMode);
     }
 
-    private void setLeftMotorRunMode(DcMotor.RunMode runMode){
+    private void setLeftMotorRunMode(DcMotor.RunMode runMode) {
         leftMotor.setMode(runMode);
     }
 
-    private void setRightMotorRunMode(DcMotor.RunMode runMode){
-        rightMotor.setMode(runMode);
+    private void setRightMotorRunMode(DcMotor.RunMode runMode) {
+        //rightMotor.setMode(runMode);
     }
 
-    void setMotorRunMode(DcMotor.RunMode runMode){
+    void setMotorRunMode(DcMotor.RunMode runMode) {
         setLeftMotorRunMode(runMode);
-        setRightMotorRunMode(runMode);
+       // setRightMotorRunMode(runMode);
     }
 
-    void balance(){
-
+    double balance(double angularVelocity) {
+        if (gyro.getHeading() == 0)
+            return 0;
+        double speed = 24 * angularVelocity / Math.cos(gyro.getHeading());
+        double revolutionPerSecond = speed / WHEEL_CIRCUMFERENCE;
+        return revolutionPerSecond;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SegwayOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SegwayOpMode.java
@@ -6,6 +6,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "Segway")
 //@Disabled
@@ -31,17 +32,59 @@ public class SegwayOpMode extends LinearOpMode{
         robot = new Hardware(hardwareMap);
 
         robot.resetEncoders();
-        robot.gyro.calibrate();
+        //robot.gyro.calibrate();
         double calibrationStartTime = getRuntime();
-        while (robot.gyro.isCalibrating() && opModeIsActive()){
-            telemetry.addData("Gyro calibrating", String.format("%1.2f", getRuntime() - calibrationStartTime));
-            telemetry.update();
-            idle();
-        }
-        telemetry.addData("Gyro calibration finished in", String.format("%1.2f seconds", getRuntime() - calibrationStartTime));
-        telemetry.update();
+        //while (robot.gyro.isCalibrating() && opModeIsActive()){
+          //  telemetry.addData("Gyro calibrating", String.format("%1.2f", getRuntime() - calibrationStartTime));
+            //telemetry.update();
+            //idle();
+        //}
+        //telemetry.addData("Gyro calibration finished in", String.format("%1.2f seconds", getRuntime() - calibrationStartTime));
+        //telemetry.update();
 
         waitForStart();
+
+        robot.setMotorRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.resetEncoders();
+
+        //double lastAngle = robot.gyro.getHeading();
+        double lastPositionLeft = robot.leftMotor.getCurrentPosition();
+        //double lastPositionRight = robot.rightMotor.getCurrentPosition();
+        double lastTime = getRuntime();
+        double angularVelocity = 0;
+        double leftRevolutionsPerSecond = 0;
+        double rightRevolutionsPerSecond = 0;
+        robot.leftMotor.setPower(.4);
+        double lastLongTimer = 0;
+        double lastLostValue = 0;
+        double averageValue = 0;
+        while (opModeIsActive()){
+            if (getRuntime() - lastLongTimer > 10){
+                averageValue = (robot.leftMotor.getCurrentPosition() - lastLostValue) / (getRuntime() - lastLongTimer);
+                lastLongTimer = getRuntime();
+                lastLostValue = robot.leftMotor.getCurrentPosition();
+            }
+          //  double currentAngle = robot.gyro.getHeading();
+            double currentLeftPosition = robot.leftMotor.getCurrentPosition();
+            //double currentRightPosition = robot.rightMotor.getCurrentPosition();
+            double timeSinceLast = getRuntime() - lastTime;
+            if (timeSinceLast > .05) {
+              //  angularVelocity = (lastAngle - currentAngle ) / timeSinceLast;
+                leftRevolutionsPerSecond = (lastPositionLeft - currentLeftPosition) / Hardware.ENCODER_TICKS_PER_REVOLUTION / timeSinceLast;
+                //rightRevolutionsPerSecond = (lastPositionRight - currentRightPosition) / timeSinceLast;
+
+                //lastAngle = currentAngle;
+                lastPositionLeft = currentLeftPosition;
+                //lastPositionRight = currentRightPosition;
+                lastTime = getRuntime();
+            }
+            telemetry.addData("RPS", leftRevolutionsPerSecond);
+            telemetry.addData("Average", averageValue / Hardware.ENCODER_TICKS_PER_REVOLUTION);
+            telemetry.update();
+            //double revolutionsPerSecond = robot.balance(angularVelocity);
+
+            idle();
+        }
 
 
     }
